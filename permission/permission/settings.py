f5c1c02541f68547f0171fb6c70a8c30a6a27b07
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-06 21:17:27 +0600

    token authentication - Login

--- permission/permission/settings.py
+++ permission/permission/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'first_app',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'user_app',
 ]
 
 MIDDLEWARE = [
